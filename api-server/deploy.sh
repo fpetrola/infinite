50cb6f7e7ec9639280e15745b106eb8007ca90ed
Fernando Petrola <fpetrola@users.noreply.github.com>
2023-03-07 02:03:42 -0300

    Update deploy.sh

--- api-server/deploy.sh
+++ api-server/deploy.sh
@@ -4,8 +4,9 @@
 # in the root of the user directory and .pem in the same place. This is what will be used
 
 set -e
+ROOT='/home/ubuntu'
 USER='infinite'
-
+GIT_HEAD=''
 SERVER=''
 DEPLOY_TYPE=$1
 
@@ -21,10 +22,10 @@ done
 
 function doDeploy {
   echo "deploying to $SERVER"
-  ssh "$USER@$SERVER" bash --login -i << EOF
+  ssh $USER@$SERVER bash --login -i << EOF
   set +e
   cd ./docker-files
-  docker-compose pull
+  docker-compose fetch
   docker-compose down
   docker-compose up -d
 
@@ -33,11 +34,28 @@ EOF
   echo "Deploy Complete To $SERVER"
 }
 
-if [[ "production" = "$DEPLOY_TYPE" ]]; then
+function set_alt_branch() {
+  if [ -n "$2" ];
+  then
+    if [[ "production" = $1 ]]; then
+      echo "Deploying alternative branches direct to production is not allowed. Is this a typo?"
+      exit
+    fi
+
+    echo "deploying alternative branch to staging server: '$2'"
+    GIT_HEAD=$2
+  fi
+}
+
+if [[ "production" = $1 ]]; then
   SERVER='infinite.industries'
+  GIT_HEAD='master'
+  set_alt_branch $1 $2
   promptUser
-elif [[ "staging" = "$DEPLOY_TYPE" ]]; then
+elif [[ "staging" = $1 ]]; then
   SERVER='staging.infinite.industries'
+  GIT_HEAD='development'
+  set_alt_branch $1 $2
   doDeploy
 else
   echo Please specify environment to deploy to.
